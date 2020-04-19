789000f76894fdb63d0f960661e70158225cff4f
ANATOLIITSYHANOV <moroko@i.ua>
2020-04-19 12:29:51 +0300

    add component-ng

--- apps/work/src/app/app.module.ts
+++ apps/work/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FormComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ apps/work/src/app/form/form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nx-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.css']
+  })
+export class FormComponent {}
