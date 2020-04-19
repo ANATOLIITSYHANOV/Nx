d34b72c5e2282230db0f6cc88a5c6904b62890c5
ANATOLIITSYHANOV <moroko@i.ua>
2020-04-19 15:57:12 +0300

    added form validation

--- apps/work/src/app/app.module.ts
+++ apps/work/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component'
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [AppComponent, FormComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- apps/work/src/app/form/form.component.ts
+++ apps/work/src/app/form/form.component.ts
@@ -1,8 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators  } from '@angular/forms';
 
 @Component({
     selector: 'nx-form',
     templateUrl: './form.component.html',
     styleUrls: ['./form.component.css']
   })
-export class FormComponent {}
+export class FormComponent implements OnInit  {
+  myFirstReactiveForm: FormGroup;
+ 
+  constructor(private fb: FormBuilder){}
+ 
+  ngOnInit(){  
+   this.initForm();
+  }
+ 
+  initForm(){
+   this.myFirstReactiveForm = this.fb.group({
+    name: [
+      'name',
+      [
+        Validators.required,
+        Validators.pattern(/[А-я]/)
+      ],
+    ],
+    email: [
+      '',
+      [
+        Validators.required, 
+        Validators.email
+      ]
+    ]
+   });
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.myFirstReactiveForm.controls[controlName];
+    const result = control.invalid && control.touched;
+    return result;
+  }
+
+  onSubmit() {
+    const controls = this.myFirstReactiveForm.controls;
+    
+     /** Проверяем форму на валидность */ 
+     if (this.myFirstReactiveForm.invalid) {
+      /** Если форма не валидна, то помечаем все контролы как touched*/
+      Object.keys(controls)
+       .forEach(controlName => controls[controlName].markAsTouched());
+       
+       /** Прерываем выполнение метода*/
+       return;
+      }
+    
+     /** TODO: Обработка данных формы */
+     console.log(this.myFirstReactiveForm.value);
+    }
+ }
