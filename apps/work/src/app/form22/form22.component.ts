ec90eff17c0888c17cd7d616d8e67eded98aae2a
ANATOLIITSYHANOV <moroko@i.ua>
2020-05-04 00:18:49 +0300

    FormBuilder

--- apps/work/src/app/form22/form22.component.ts
+++ apps/work/src/app/form22/form22.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: "nx-form22",
   templateUrl: './form22.component.html',
 })
 export class Form22Component {
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
   });
+
+  constructor(private fb: FormBuilder) { }
 
   onSubmit() {
     console.warn(this.profileForm.value);
